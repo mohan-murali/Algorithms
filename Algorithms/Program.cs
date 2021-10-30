ff35a9b5ab198596f4f1ed7a2cea96cf9a9fbfc8
mohan-murali <mohanmuralid28@gmail.com>
2021-10-30 07:42:42 +0530

    uncommented the commented code

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -9,10 +9,10 @@ namespace Algorithms
         static void Main(string[] args)
         {
             //Sort function tests
-            // TestSort.Test(BubbleSort.Sort, "Bubble Sort");
-            // TestSort.Test(SelectionSort.Sort, "Selection Sort");
-            // TestSort.Test(MergeSort.Sort, "Merge Sort");
-            // TestSort.Test(QuickSort.Sort, "Quick Sort");
+            TestSort.Test(BubbleSort.Sort, "Bubble Sort");
+            TestSort.Test(SelectionSort.Sort, "Selection Sort");
+            TestSort.Test(MergeSort.Sort, "Merge Sort");
+            TestSort.Test(QuickSort.Sort, "Quick Sort");
             
             //Search function tests
             TestSearch.Test(LinearSearch.Search, "Linear Search");
